18acb6b60fde162db554a0e19c72517796395472
mouaz43 <mouazmjr51@icloud.com>
2025-09-09 01:22:15 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-// Year + menu logic
 document.addEventListener('DOMContentLoaded', () => {
   const y = document.getElementById('year'); if (y) y.textContent = new Date().getFullYear();
   initMenu();
